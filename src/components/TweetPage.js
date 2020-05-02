238a65c5d3eaeed50c4ca096edfb323e98bbc318
Ram Krishna <krishram101@gmail.com>
2020-05-02 23:39:20 +0530

    Adding Twitter Chirper application

---
+++ src/components/TweetPage.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import Tweet from "./Tweet";
+import NewTweet from "./NewTweet";
+
+class TweetPage extends Component {
+  render() {
+    const { id, replies } = this.props;
+
+    return (
+      <div>
+        <Tweet id={id} />
+        <NewTweet id={id} />
+        {replies.length && <h3 className="center">Replies</h3>}
+        <ul>
+          {replies.map(replyId => (
+            <li key={replyId}>
+              <Tweet id={replyId} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+function mapStateToProps({ authedUser, tweets, users }, props) {
+  const { id } = props.match.params;
+  return {
+    id,
+    authedUser,
+    replies: !tweets[id]
+      ? []
+      : tweets[id].replies.sort(
+          (a, b) => tweets[b].timestamp - tweets[a].timestamp
+        )
+  };
+}
+
+export default connect(mapStateToProps)(TweetPage);
